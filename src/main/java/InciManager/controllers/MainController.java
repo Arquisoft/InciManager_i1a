83c3c0cafee77de0d68dd4ce4255915346502f14
uo251851 <marcialfco@live.com>
2018-03-20 11:54:11 +0100

    Added .properties to connect mongodb.

--- src/main/java/InciManager/controllers/MainController.java
+++ src/main/java/InciManager/controllers/MainController.java
@@ -55,4 +55,28 @@ public class MainController {
         return "redirect:/";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPost(Model model, @RequestParam(value = "login") String id, @RequestParam(value = "password") String password,
+                            @RequestParam(value = "kind") String kind) {
+        // If the combination of id and password is correct, the data of the user is returned
+        // If not, 404 NOT FOUND is returned
+        AgentInfo user = service.getAgent(id, password, Integer.parseInt(kind));
+
+        if (user == null)
+            return "usererror";
+        else {
+            model.addAttribute( "name", user.getName());
+            model.addAttribute( "location", user.getLocation());
+            model.addAttribute( "email", user.getEmail());
+            model.addAttribute( "id", user.getId());
+            model.addAttribute( "kindCode", user.getKind());
+            if (user.getKind() == 1) model.addAttribute( "kind", "person");
+            else if (user.getKind() == 2) model.addAttribute( "kind", "entity");
+            else if (user.getKind() == 3) model.addAttribute( "kind", "sensor");
+
+            return "getAgentInfo";
+        }
+
+    }
+
 }
