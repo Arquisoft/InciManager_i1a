83af6caf6d14a182b27c1dda1bf9549b1dd02d78
alexgonzcampomanes <alexdg365@gmail.com>
2018-03-24 20:21:41 +0100

    Launch application fixed.

--- src/test/java/app/MainControllerTest.java
+++ src/test/java/app/MainControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
-import app.MainApplication;
+import com.app.MainApplication;
 
 
 
