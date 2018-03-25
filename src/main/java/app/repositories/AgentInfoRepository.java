f488c044fd3a843f742e9757c2c827b54c233b9f
alexgonzcampomanes <alexdg365@gmail.com>
2018-03-25 22:10:53 +0200

    Commit hierarchy of package changed.

--- src/main/java/app/repositories/AgentInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import entities.AgentInfo;
-
-public interface AgentInfoRepository extends CrudRepository<AgentInfo, String>{
-}
--- src/main/java/app/services/TopicsService.java
+++
@@ -1,8 +0,0 @@
-package services;
-
-public class TopicsService {
-    private String[] topics={"ACCIDENT","FIRE","ALTERCATION","MEDICAL_EMERGENCY","METEOROLOGICAL_PHENOMENON","OTHER"};
-    public String[] getTopics(){
-        return topics.clone();
-    }
-}
