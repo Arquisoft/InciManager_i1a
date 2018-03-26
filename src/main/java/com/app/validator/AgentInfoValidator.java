0904e87e5ca6f12117e5366e19121fff3605deb8
uo251851 <marcialfco@live.com>
2018-03-26 20:37:38 +0200

    Added examples in the create incident form.

--- src/main/java/com/app/validator/AgentInfoValidator.java
+++ src/main/java/com/app/validator/AgentInfoValidator.java
@@ -26,6 +26,7 @@ public class AgentInfoValidator implements Validator {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "error.empty");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "error.empty");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "kind", "error.empty");
+        
         Agent posibleAgent = agentInfoService.findById( agentInfo.getId() );
         if ( posibleAgent == null) {
         	errors.rejectValue("id", "error.user.identification");
