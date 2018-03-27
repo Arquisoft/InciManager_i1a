7963e3f46ed6f303bb785bcc6025d483238953ac
Lucía Méndez López <UO250970@uniovi.es>
2018-03-27 16:55:57 +0200

    Finished Entities tests

--- src/test/java/app/EntitiesTest.java
+++ src/test/java/app/EntitiesTest.java
@@ -46,38 +46,42 @@ public class EntitiesTest {
 
 	@Test
 	public void testAgent() throws Exception {
-		String password;
-        String name;
-        String email;
-        String location;
-        String NIF;
-        int kind;
-
-        name = "name";
-        password = "password";
-        email = "mail@mail.com";
-        location = "caceres";
-        NIF = "10203040A";
-        kind = 1;
+        String name = "name";
+		String password = "password";
+        String email = "mail@mail.com";
+        String location = "caceres";
+        String NIF = "10203040A";
+        int kind = 1;
 
         Agent user = new Agent(name,email,password,NIF,kind);
         Agent user2 = new Agent(name,email,password,location,NIF,kind);
+        
+        String[] valores = {name, password, email, location, NIF, String.valueOf(kind)};
+        Agent user3 = new Agent(valores);
 
-        assertTrue(user.getPassword().equals(password));
         assertTrue(user.getName().equals(name));
+        assertTrue(user.getPassword().equals(password));
         assertTrue(user.getId().equals(NIF));
         assertTrue(user.getEmail().equals(email));
         assertTrue(user.getLocation().equals(""));
         assertTrue(user.getIdautogenerado() == null);
         assertTrue(user.getKind() == 1);
 
-        assertTrue(user2.getPassword().equals(password));
         assertTrue(user2.getName().equals(name));
+        assertTrue(user2.getPassword().equals(password));
         assertTrue(user2.getId().equals(NIF));
         assertTrue(user2.getEmail().equals(email));
         assertTrue(user2.getLocation().equals(location));
         assertTrue(user2.getIdautogenerado() == null);
         assertTrue(user2.getKind() == 1);
+        
+        assertTrue(user3.getName().equals(name));
+        assertTrue(user3.getPassword().equals(password));
+        assertTrue(user3.getId().equals(NIF));
+        assertTrue(user3.getEmail().equals(email));
+        assertTrue(user3.getLocation().equals(location));
+        assertTrue(user3.getIdautogenerado() == null);
+        assertTrue(user3.getKind() == 1);
 
         assertEquals("Agent [idautogenerado='null', name='name'" +
                 ", email='mail@mail.com', password='password', location='', id='10203040A'" +
@@ -106,7 +110,9 @@ public class EntitiesTest {
         assertEquals("Agent [idautogenerado='null', name=''" +
                 ", email='', password='', location='Málaga', id=''" +
                 ", kind=2]", user.toString());
-        
+
+        assertFalse(user.equals(null));
+        assertTrue(user.equals(user));
         assertFalse(user.hashCode() == user2.hashCode());
         assertFalse(user.equals(user2));
         
@@ -154,7 +160,7 @@ public class EntitiesTest {
 		incident.setTags(tags);
 		assertNotNull(incident.getTags());
 		assertTrue(tags.equals(incident.getTags()));
-		assertTrue(incident.getTags().size() == 2);
+		assertTrue(incident.getTags().size() == tags.size());
 		
 		operator.setUsername("TesterOperator");
 		incident.setOperator(operator);
@@ -191,10 +197,49 @@ public class EntitiesTest {
 		incident.setStatus(status);
 		assertNotNull(incident.getStatus());
 		assertTrue(status.equals(incident.getStatus()));
+
+		assertFalse(incident.equals(null));
+		assertTrue(incident.equals(incident));
 		
 		Incident incident2 = new Incident();
 		assertFalse(incident.hashCode() == incident2.hashCode());
 		assertFalse(incident.equals(incident2));
 	}
 
+	@Test
+	public void testOperator() {
+		String username = "Operator1";
+		String password = "123456";
+		List<Incident> incidents = new ArrayList<Incident>();
+		
+		Operator operator1 = new Operator(username, password);
+		assertEquals("Operator [id='null', username='Operator1', password='123456', incidents='[]']", operator1.toString());
+		
+		Operator operator2 = new Operator();
+		assertEquals("Operator [id='null', username='null', password='null', incidents='[]']", operator2.toString());
+		
+		String username2 = "Operator2";
+		operator2.setUsername(username2);
+		assertNotNull(operator2.getUsername());
+		assertTrue(username2.equals(operator2.getUsername()));
+		
+		String password2 = "654321";
+		operator2.setPassword(password2);
+		assertNotNull(operator2.getPassword());
+		assertTrue(password2.equals(operator2.getPassword()));
+		
+		incidents.add(new Incident());
+		incidents.add(new Incident());
+		operator2.setIncidents(incidents);
+		assertNotNull(operator2.getIncidents());
+		assertTrue(incidents.equals(operator2.getIncidents()));
+		assertTrue(operator2.getIncidents().size() == incidents.size());
+		
+		assertFalse(operator1.equals(null));
+		assertTrue(operator1.equals(operator1));
+		
+		assertFalse(operator1.hashCode() == operator2.hashCode());
+		assertFalse(operator1.equals(operator2));
+	}
+	
 }
