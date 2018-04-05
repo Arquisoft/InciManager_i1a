fe1cff9c5d0df9107997113384379160ff2c0021
uo251851 <marcialfco@live.com>
2018-04-05 10:39:24 +0100

    Solved problem in AgentInfoService.

--- src/main/java/com/app/services/AgentInfoService.java
+++ src/main/java/com/app/services/AgentInfoService.java
@@ -1,11 +1,16 @@
 package com.app.services;
 
-import org.jboss.netty.handler.codec.http.HttpMethod;
+import java.io.IOException;
+import java.util.List;
+
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,42 +22,33 @@ import com.app.repositories.AgentInfoRepository;
 
 @Service
 public class AgentInfoService {
-	
+
 	@Value("http://localhost:8080/login")
-	private String url;
-
+	private String agentsUrl;
+	
 	@Autowired
-	AgentInfoRepository agentInfoRepository;
-
-	public AgentInfo findById(String id) {
-		return agentInfoRepository.findById(id);
-	}
-
-	
-	public String getLocation(AgentInfo agentInfo) {
-		// TODO
-		return "";
-	}
-	
+	private AgentInfoRepository agentInfoRepository;
+		
 	public boolean verifyAgent(AgentInfo agentInfo) {
 		HttpStatus response;
 		
 		try {
 			HttpHeaders header = new HttpHeaders();
 			header.setContentType(MediaType.APPLICATION_JSON);
-			
+
 			JSONObject request = new JSONObject();
 			request.put("login", agentInfo.getId());
-			request.put("password", agentInfo.getPassword());
+	        request.put("password", agentInfo.getPassword());
 			request.put("kind", agentInfo.getKind());
-			
-			HttpEntity<String> entity = new HttpEntity<>(request.toString(), header);
-			response = this.getResponseStatus(url, HttpMethod.POST, entity);
+
+			HttpEntity<String> entity = new HttpEntity<String>(request.toString(), header);
+			response = this.getResponseStatus(agentsUrl, HttpMethod.POST, entity);
+		
 		} catch (Exception e) {
 			return false;
 		}
 		
-		return response.equals(HttpStatus.OK);
+        return response.equals(HttpStatus.OK);
 	}
 	
 	public HttpStatus getResponseStatus(String url, HttpMethod method, HttpEntity<String> entity) {
